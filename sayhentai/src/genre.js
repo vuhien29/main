7a91a519a255801b604e730aac27e4ed620f2e1d
vuhien29 <hienytb29@gmail.com>
2025-03-07 23:56:04 +0700

    Add files via upload

---
+++ sayhentai/src/genre.js
@@ -0,0 +1,320 @@
+function execute() {
+    return Response.success([
+        {
+            "input": "/genre/18/",
+            "title": "18+",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/3d/",
+            "title": "3D",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/action/",
+            "title": "Action",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/adult/",
+            "title": "Adult",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/bao-dam/",
+            "title": "Bạo Dâm",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/choi-hai-lo/",
+            "title": "Chơi hai lỗ",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/comedy/",
+            "title": "Comedy",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/detective/",
+            "title": "Detective",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/doujinshi/",
+            "title": "Doujinshi",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/drama/",
+            "title": "Drama",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/ecchi/",
+            "title": "Ecchi",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/fantasy/",
+            "title": "Fantasy",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/gangbang/",
+            "title": "Gangbang",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/gender-bender/",
+            "title": "Gender Bender",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/giao-vien/",
+            "title": "Giáo viên",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/group/",
+            "title": "Group",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/ham-hiep/",
+            "title": "Hãm hiếp",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/harem/",
+            "title": "Harem",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/hau-mon/",
+            "title": "Hậu môn",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/historical/",
+            "title": "Historical",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/horror/",
+            "title": "Horror",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/housewife/",
+            "title": "Housewife",
+            "script": "gen.js"
+        },
+      {
+            "input": "/genre/Josei/",
+            "title": "Josei",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/khong-che/",
+            "title": "Không che",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/kinh-di/",
+            "title": "Kinh dị",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/lao-gia-dam/",
+            "title": "Lão già dâm",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/loan-luan/",
+            "title": "Loạn luân",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/loli/",
+            "title": "Loli",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/manga/",
+            "title": "manga",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/manhua/",
+            "title": "Manhua",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/manhwa/",
+            "title": "Manhwa",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/martisl-arts/",
+            "title": "Martial arts",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/mature/",
+            "title": "Mature",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/milf/",
+            "title": "Milf",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/mind-break/",
+            "title": "Mind break",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/nguc-lon/",
+            "title": "Ngực lớn",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/nguc-nho/",
+            "title": "Ngực nhỏ",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/no-le/",
+            "title": "Nô lệ",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/ntr/",
+            "title": "NTR",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/nu-sinh/",
+            "title": "Nữ Sinh",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/old-man/",
+            "title": "Old Man",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/one-shot/",
+            "title": "One shot",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/oneshot/",
+            "title": "Oneshot",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/psychological/",
+            "title": "Psychological",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/rape/",
+            "title": "Rape",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/romance/",
+            "title": "Romance",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/school-life/",
+            "title": "School life",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/sci-fi/",
+            "title": "Sci-fi",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/seinen/",
+            "title": "Seinen",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/series/",
+            "title": "Series",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/shoujo/",
+            "title": "Shoujo",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/shoujo-ai/",
+            "title": "Shoujo AI",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/shounen/",
+            "title": "Sports",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/slice-of-life/",
+            "title": "Slice of life",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/smut/",
+            "title": "Smut",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/sports/",
+            "title": "Sports",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/supernatural/",
+            "title": "Supernatural",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/tragedy/",
+            "title": "Tragedy",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/virgin/",
+            "title": "Virgin",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/webtoon/",
+            "title": "Webtoon",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/y-ta/",
+            "title": "Y tá",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/yaoi/",
+            "title": "Yaoi",
+            "script": "gen.js"
+        },
+        {
+            "input": "/genre/yuri/",
+            "title": "Yuri",
+            "script": "gen.js"
+        }
+          
+    ]);
+}
---
+++ sayhentai/src/home.js
@@ -0,0 +1,10 @@
+load('config.js');
+function execute() {
+    return Response.success([
+        { title: "Cập Nhật", input: BASE_URL + "/", script: "gen.js" },
+        { title: "Manhwa", input: BASE_URL + "/genre/manhwa", script: "gen.js" },
+        { title: "Manga", input: BASE_URL + "/genre/manga", script: "gen.js" },
+        { title: "Manhua", input: BASE_URL + "/genre/manhua", script: "gen.js" },
+        { title: "Thể loại", input: BASE_URL + "/genre/genre", script: "gen.js" },
+    ]);
+}
