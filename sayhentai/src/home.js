931b9b5b2feb2203eb290cc715acfebae96bb091
vuhien29 <hienytb29@gmail.com>
2025-03-08 00:39:29 +0700

    Update home.js

--- sayhentai/src/home.js
+++ sayhentai/src/home.js
@@ -2,9 +2,6 @@ load('config.js');
 function execute() {
     return Response.success([
         { title: "Cập Nhật", input: BASE_URL + "/", script: "gen.js" },
-        { title: "Manhwa", input: BASE_URL + "/genre/manhwa", script: "gen.js" },
-        { title: "Manga", input: BASE_URL + "/genre/manga", script: "gen.js" },
-        { title: "Manhua", input: BASE_URL + "/genre/manhua", script: "gen.js" },
         { title: "Loli", input: BASE_URL + "/genre/loli", script: "gen.js" },
         { title: "Loạn luân", input: BASE_URL + "/genre/loan-luan", script: "gen.js" },
     ]);
